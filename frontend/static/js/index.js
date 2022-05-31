5e0346a9d7c2adc6c7483209a376595e8bb7893c
David Bryant <nedink@gmail.com>
2022-05-31 01:32:58 -0400

    load more board

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -1,15 +1,28 @@
 
+// ON STARTUP
+// const target_words = [
+//     'coffee', 'tea', 'toast', 'milk', 'juice'
+// ]
+// document.getElementById('target-words').textContent = target_words.join(' ')
+const target_words = document.getElementById('target-words').textContent.split(' ')
+
 let clicks = 0
+
+
 
 const onWordOptionClick = async event => {
     const prompt = event.srcElement.textContent
     console.log(prompt)
 
-    const target_word = document.getElementById('target')
     const word_options = document.getElementById('word-options')
-    if (prompt.includes(target_word.textContent)) {
+    const click_count = document.getElementById('click-count')
+
+    click_count.innerText = `clicks: ${++clicks}`
+
+    let found_word
+    if (found_word = target_words.find(v => prompt === v)) {
         word_options.innerHTML = `
-        <div><h1>YOU WIN!</h1></div>
+        <h1>You found <span class="special">${found_word}</span>!</h1>
         `
 
         // OPEN SCORE SUBMISSION
@@ -28,14 +41,14 @@ const onWordOptionClick = async event => {
         `
         document.getElementById('submit-score-form').onsubmit = event => {
             document.getElementById('submit-score-div').innerHTML = ``
-            onSubmitScore(event)
+            onSubmitScore(event, found_word)
         }
         return
     }
 
     word_options.innerHTML = `<div style="display: flex; align-items: center; justify-content: center;"><div class="loader"></div></div>`
 
-    const res = await fetch('/prompt', {
+    const res = await (await fetch('/prompt', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -47,27 +60,23 @@ const onWordOptionClick = async event => {
         // mode: 'cors', // no-cors or cors 
         // cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
         // credentials: 'same-origin ',// include , same – origin 
-    })
+    })).json()
 
-    const res_json = await res.json()
+    // const res_json = await res.json()
 
-    let choices = [...new Set(res_json.map(ch => ch.text))]
+    let choices = [...new Set(res.map(ch => ch.text))]
     console.log(choices)
     choices = choices.filter(choice => choice.trim())
 
     word_options.innerHTML = ``
     for (let choice of choices) {
-        word_options.innerHTML += `<div id="word-option-1" class="word-option special">${choice}</div>`
+        word_options.innerHTML += `<div class="word-option">${choice}</div>`
     }
 
     const wordOptionEls = document.getElementsByClassName('word-option')
     for (let i = 0; i < wordOptionEls.length; i++) {
         wordOptionEls[i].onclick = onWordOptionClick
     }
-
-    const click_count = document.getElementById('click-count')
-    click_count.innerText = `Clicks - ${++clicks}`
-
 };
 
 const wordOptionEls = document.getElementsByClassName('word-option')
@@ -76,9 +85,10 @@ for (let i = 0; i < wordOptionEls.length; i++) {
 }
 
 
+let leaderboard_i = 0
 
 // SUBMIT SCORE
-const onSubmitScore = async (event) => {
+const onSubmitScore = async (event, found_word) => {
     event.preventDefault();
 
     // const submitScoreEl = document.getElementById('submit-score')
@@ -88,19 +98,23 @@ const onSubmitScore = async (event) => {
 
     const name = data.get('name');
 
-    const res = await (await fetch('/leaderboard', {
+    const placement = (await (await fetch('/leaderboard', {
         method: 'POST',
         headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
         },
         body: JSON.stringify({
             name: name,
+            word: found_word,
             clicks: clicks,
         })
         // mode: 'cors', // no-cors or cors 
         // cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
         // credentials: 'same-origin ',// include , same – origin 
-    })).json()
+    })).json()).placement
+
+    const res = await (await fetch('/leaderboard?i=0')).json()
 
     const leaderboard = res.leaderboard
 
@@ -108,17 +122,22 @@ const onSubmitScore = async (event) => {
 
     const rowsHTML = leaderboard.map(o => `
     <tr>
-        <td>${o.name}</td>
-        <td>${o.clicks}</td>
+        <td class="td-i">${++leaderboard_i}.</td>
+        <td class="td-name">${o.name}</td>
+        <td class="td-word">${o.word}</td>
+        <td class="td-clicks">${o.clicks}</td>
     </tr>
     `).reduce((a, b) => a + b)
 
-    document.getElementById('leaderboard').innerHTML = `
-    <div style="background-color: antiquewhite;">#${res.placement}</div><div></div>
+    document.getElementById('leaderboard-container').innerHTML = `
+    <div>${name}: #${placement}</div><div></div>
     <table id="leaderboard">
-        <thead>
+        <thead style="width: 100%">
             <tr>
-                <th>Leaderboard</th>
+                <th class="td-i"></th>
+                <th class="td-name">Name</th>
+                <th class="td-word">Word</th>
+                <th class="td-clicks">Clicks</th>
             </tr>
         </thead>
         <tbody id="leaderboard-table-body">
@@ -127,5 +146,38 @@ const onSubmitScore = async (event) => {
     </table>
     `
 
-    // console.log(res);
+    if (res.end) {
+        return
+    }
+
+    const nextTenButton = document.createElement('input')
+    nextTenButton.setAttribute('id', 'next-ten')
+    nextTenButton.setAttribute('type', 'button')
+    nextTenButton.setAttribute('value', '...')
+    const nextTenButtonEl = document.getElementById('scoring').appendChild(nextTenButton)
+    nextTenButtonEl.onclick = nextTen
+}
+
+
+const nextTen = async event => {
+    // const rowEls = []
+    const tableBody = document.getElementById('leaderboard-table-body')
+
+    const json = await (await fetch(`/leaderboard?i=${leaderboard_i}`)).json()
+    const leaderboard = json.leaderboard
+    const end = json.end
+
+    for (let o of leaderboard) {
+        const newRow = document.createElement('tr')
+        newRow.innerHTML = `
+        <td class="td-i">${++leaderboard_i}</td>
+        <td class="td-name">${o.name}</td>
+        <td class="td-word">${o.word}</td>
+        <td class="td-clicks">${o.clicks}</td>`
+        tableBody.appendChild(newRow)
+    }
+    if (leaderboard_i >= leaderboard.length) {
+        event.srcElement.remove()
+    }
+
 }
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path')
+const fs = require('fs')
 require('dotenv').config()
 
 // DECLARE AND CONFIGURE SERVER
@@ -12,7 +13,7 @@ app.use(express.json())
 // Whenever the path has '/static' in it, serve the `static/` dir as usual
 app.use('/static', express.static(path.resolve(__dirname, 'frontend', 'static')))
 // For any path, go to the root and send back index.html
-app.get('/*', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'frontend', 'index.html'))
 })
 
@@ -24,45 +25,37 @@ listener = app.listen(process.env.PORT || 3000, () => console.log(`Server runnin
 
 // SCORING
 const leaderboard = []
-leaderboard.push(
-    {
-        name: 'doug',
-        clicks: 4,
-    },
-    {
-        name: 'david',
-        clicks: 3,
-    },
-    {
-        name: 'isabel',
-        clicks: 10,
-    },
-)
 
 app.post('/leaderboard', (req, res) => {
     // return the leaderboard top 10 and where user has placed
     const score = req.body
-    
-    console.log(score)
+    // const i = req.query.i
 
     leaderboard.push(score)
-    
     leaderboard.sort((a, b) => a.clicks - b.clicks)
     
-    const placement = leaderboard.indexOf(score)
-
+    const placement = leaderboard.indexOf(score) + 1
     const body = {
-        placement: placement,
-        leaderboard: leaderboard.slice(0, 10),
+        placement: placement
     }
 
     res.send(body)
+})
+
+app.get('/leaderboard', (req, res) => {
+    // return the next 10 in the leaderboard
+    const i = Number(req.query.i)
+    res.send({
+        leaderboard: leaderboard.slice(i, Math.min(i + 10, leaderboard.length)),
+        end: i + 10 >= leaderboard.length
+    })
 })
 
 
 
 // OPENAI CALLS
 const { Configuration, OpenAIApi } = require("openai");
+const { fstat } = require('fs')
 
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
@@ -71,7 +64,7 @@ const openai = new OpenAIApi(configuration);
 
 const complete = async (p) => {
 
-    console.log('complete(): prompt is...' + p)
+    console.log('Completing from... ' + p)
 
     const completion = await openai.createCompletion(process.env.MODEL_2, {
         prompt: p + ":",
@@ -87,10 +80,6 @@ const complete = async (p) => {
         // console.log(e)
     });
 
-    // completion.data.choices[0]
-    // console.log(completion)
-    // console.log(completion.data.choices.slice(0, 3));
-
     const choices = completion.data.choices
         .map(choice => {
             choice.text = choice.text.trim()
@@ -104,12 +93,9 @@ const complete = async (p) => {
 // ENDPOINT TO REQUEST NEW WORDS
 app.post('/prompt', async (req, res) => {
     chosen_word = req.body.prompt
-
-    console.log(chosen_word)
-
     const choices = await complete(chosen_word)
-
-    console.log(choices)
-
     res.send(choices)
 })
+
+
+// SEND THE DAILY WORDS
